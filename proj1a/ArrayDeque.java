745a5c0fbf392bce5a468e96eb998f578177bec6
zxc <guilongzhongwei@hotmail.com>
2025-03-08 16:02:45 +0800

    完成ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,133 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private static final int INITIAL_CAPACITY = 8;
+    private static final float LOAD_FACTOR = 0.25f;
+    private T[] array;
+    private int size;
+    private int capacity;
+    private int nextFirst;
+    private int nextLast;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayDeque() {
+        array = (T[]) new Object[INITIAL_CAPACITY];
+        capacity = INITIAL_CAPACITY;
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void copyArray(T[] newArray) {
+        for (int i = 0; i < size; i++) {
+            newArray[i] = get(i);
+        }
+        array = newArray;
+    }
+
+    private void expand() {
+        if (size == capacity) {
+            T[] newArray = (T[]) new Object[capacity * 2];
+            copyArray(newArray);
+            capacity *= 2;
+            nextFirst = capacity - 1;
+            nextLast = size;
+        }
+    }
+
+    private void shrink() {
+        if ((capacity != INITIAL_CAPACITY) && ((double) size / (double) capacity < LOAD_FACTOR)) {
+            T[] newArray = (T[]) new Object[capacity / 2];
+            copyArray(newArray);
+            capacity /= 2;
+            nextFirst = capacity - 1;
+            nextLast = size;
+        }
+    }
+
+    public void addFirst(T value) {
+        expand();
+
+        array[nextFirst] = value;
+        if (nextFirst == 0) {
+            nextFirst = capacity - 1;
+        } else {
+            nextFirst--;
+        }
+
+        size++;
+    }
+
+    public void addLast(T value) {
+        expand();
+
+        array[nextLast] = value;
+        if (nextLast == capacity - 1) {
+            nextLast = 0;
+        } else {
+            nextLast++;
+        }
+
+        size++;
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+
+        if (nextFirst == capacity - 1) {
+            nextFirst = 0;
+        } else {
+            nextFirst++;
+        }
+        T value = array[nextFirst];
+        size--;
+
+        shrink();
+
+        return value;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+
+        if (nextLast == 0) {
+            nextLast = capacity - 1;
+        } else {
+            nextLast--;
+        }
+        T value = array[nextLast];
+        size--;
+
+        shrink();
+
+        return value;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+
+        int i = (nextFirst + index + 1) % capacity;
+        return array[i];
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) + " ");
+        }
+    }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,2 +1,148 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ArrayDequeTest {
+    @Test
+    public void testArrayDequeConstructor() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        assertTrue(deque.isEmpty());
+        assertEquals(0, deque.size());
+        assertEquals(8, deque.getCapacity());
+        assertNull(deque.removeFirst());
+        assertNull(deque.removeLast());
+        assertNull(deque.get(0));
+    }
+
+    @Test
+    public void testAddFirst() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        // 4 3 2 1
+        deque.printDeque();
+        assertFalse(deque.isEmpty());
+        assertEquals(4, deque.size());
+        assertEquals(8, deque.getCapacity());
+        assertEquals(4, (int)deque.get(0));
+        assertEquals(2, (int)deque.get(2));
+    }
+
+    @Test
+    public void testAddLast() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        // 1 2 3 4
+        deque.printDeque();
+        assertFalse(deque.isEmpty());
+        assertEquals(4, deque.size());
+        assertEquals(8, deque.getCapacity());
+        assertEquals(1, (int)deque.get(0));
+        assertEquals(3, (int)deque.get(2));
+    }
+
+    @Test
+    public void testRemoveAndAdd() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addLast(4);
+        deque.addLast(5);
+        deque.addLast(6);
+        // 3 2 1 4 5 6
+        deque.printDeque();
+        assertFalse(deque.isEmpty());
+        assertEquals(6, deque.size());
+        assertEquals(4, (int)deque.get(3));
+        assertEquals(3, (int)deque.removeFirst());
+        assertEquals(2, (int)deque.removeFirst());
+        assertEquals(6, (int)deque.removeLast());
+        assertEquals(5, (int)deque.removeLast());
+        assertEquals(2, deque.size());
+        assertEquals(4, (int)deque.get(1));
+        deque.addFirst(7);
+        deque.addFirst(8);
+        deque.addLast(9);
+        deque.addLast(10);
+        // 8 7 1 4 9 10
+        deque.printDeque();
+        assertEquals(6, deque.size());
+        assertEquals(10, (int)deque.get(5));
+    }
+
+    @Test
+    public void testAddFirstExpand() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < 8; i++) {
+            deque.addFirst(i);
+        }
+        assertEquals(8, deque.getCapacity());
+
+        deque.addFirst(8);
+        // 8 7 6 5 4 3 2 1 0
+        deque.printDeque();
+        assertEquals(16, deque.getCapacity());
+        assertEquals(8, (int)deque.get(0));
+    }
+
+    @Test
+    public void testAddLastExpand() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < 8; i++) {
+            deque.addLast(i);
+        }
+        assertEquals(8, deque.getCapacity());
+
+        deque.addLast(8);
+        // 0 1 2 3 4 5 6 7 8
+        deque.printDeque();
+        assertEquals(16, deque.getCapacity());
+        assertEquals(8, (int)deque.get(8));
+    }
+
+    @Test
+    public void testRemoveFirstShrink() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < 9; i++) {
+            deque.addFirst(i);
+        }
+        assertEquals(16, deque.getCapacity());
+
+        for(int i = 0; i < 5; i++) {
+            deque.removeFirst();
+        }
+        assertEquals(16, deque.getCapacity());
+
+        deque.removeFirst();
+        assertEquals(8, deque.getCapacity());
+        deque.removeFirst();
+        assertEquals(8, deque.getCapacity());
+        assertEquals(1, (int)deque.get(0));
+    }
+
+    @Test
+    public void testRemoveLastShrink() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < 9; i++) {
+            deque.addLast(i);
+        }
+        assertEquals(16, deque.getCapacity());
+
+        for(int i = 0; i < 5; i++) {
+            deque.removeLast();
+        }
+        assertEquals(16, deque.getCapacity());
+
+        deque.removeLast();
+        assertEquals(8, deque.getCapacity());
+        deque.removeLast();
+        assertEquals(8, deque.getCapacity());
+        assertEquals(0, (int)deque.get(0));
+    }
 }
